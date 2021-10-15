d7dfdae3a19e43d9d80abc1cb261e4bfa401f8a1
Tharusha Perera <87405522+IT21057724@users.noreply.github.com>
2021-10-15 21:18:08 +0530

    Add files via upload

---
+++ IT21031366/Developer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+#include "Developer.h"
+using namespace std;
+Developer::Developer(char developername ,char fName[], char lName[], char Email[], char pw[], char devName[]) : User(fName, lName, Email, pw)
+{
+    developername = devName;
+}
+{
+    void Developer::uploadApp()
+}
+{
+    void Developer::updateApp()
+}
+{
+    void Developer::deleteApp()
+}
+{
+    Developer::~Developer()
+}
